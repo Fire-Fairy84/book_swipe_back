c463bd2c9ac5405549907fd0c2715f70af1725bd
Fire-Fairy84 <esther.sarasua@gmail.com>
2024-09-07 21:12:25 +0200

    create function to get both user_id and token

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RegisterView, LogoutView, UserViewSet
-from rest_framework.authtoken.views import obtain_auth_token
+from .views import RegisterView, LogoutView, UserViewSet, CustomAuthToken
+
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', obtain_auth_token, name='login'),
+    path('login/', CustomAuthToken.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('', include(router.urls)),
 ]
