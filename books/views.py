14838b21757b69648b8b868facac4e8a3659dbd6
Fire-Fairy84 <esther.sarasua@gmail.com>
2024-09-09 00:58:29 +0200

    add mybooks url and mybooks view

--- books/views.py
+++ books/views.py
@@ -2,9 +2,20 @@ from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
+
+
+class MyBooksView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user_books = Book.objects.filter(user=request.user)
+        serializer = BookSerializer(user_books, many=True)
+        return Response(serializer.data)
